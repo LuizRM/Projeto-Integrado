9bcb103b650f005e3e1498172f5d439195941492
Luiz Morais <luizg.rmo@gmail.com>
2022-02-07 16:01:42 -0300

    Adicionou cors e controle de usuarios no info

--- controllers/infoControllers.js
+++ controllers/infoControllers.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const User = require("../models/user");
+const authMiddleware = require("../middlewares/authkey");
+const res = require("express/lib/response");
 
 
 const router = express.Router();
+router.use(authMiddleware);
 
 router.post("/update", async (req, res) => {
     const {idG, newName} = req.body;
@@ -13,6 +16,19 @@ router.post("/update", async (req, res) => {
     console.log(retorno.modifiedCount);
     res.send("OK");
 
+});
+
+router.delete("/", async (req, res) => {
+    try{
+        const retorno = await User.findByIdAndDelete(req.userId);
+        if(retorno){
+            console.log(retorno);
+            return res.status(200).send("User deleted!");
+        }
+    }
+    catch(err) {
+        return res.status(500).send(err);
+    }
 });
 
 module.exports = app => app.use("/info", router);
